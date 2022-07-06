dc97e72a83626120dc3aefd048cde991c8b55d73
Pascal Martineau <pascal@lewebsimple.ca>
2022-07-05 21:06:20 -0400

    feat: Configure FontAwesome free auto-import

--- templates/default/nuxt.config.ts
+++ templates/default/nuxt.config.ts
@@ -3,7 +3,10 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   components: {
-    dirs: [{ path: "~/components", extensions: ["svg", "vue"] }],
+    dirs: [
+      { path: "~/components", extensions: ["svg", "vue"] },
+      { path: "node_modules/@fortawesome/fontawesome-free/svgs/regular", extensions: ["svg"], prefix: "Icon" },
+    ],
   },
   modules: ["@lewebsimple/nuxt3-svg", "@nuxtjs/tailwindcss"],
   tailwindcss: {
