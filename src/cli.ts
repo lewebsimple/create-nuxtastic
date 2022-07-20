1a166e9799395746d3286f9a548b576fdb883a1d
Pascal Martineau <pascal@lewebsimple.ca>
2022-07-20 11:34:26 -0400

    feat: Configure DB / admin email / password

--- src/cli.ts
+++ src/cli.ts
@@ -11,4 +11,12 @@ create("create-nuxtastic", {
   caveat: ({ packageDir }) => {
     console.log(`cd ${packageDir} && yarn dev`);
   },
+  extra: {
+    dbhost: { type: "input", describe: "MySQL hostname", default: "localhost" },
+    dbport: { type: "input", describe: "MySQL port", default: "3306" },
+    dbuser: { type: "input", describe: "MySQL username", default: "root" },
+    dbname: { type: "input", describe: "MySQL database" },
+    seedemail: { type: "input", describe: "Admin email", default: "admin@example.com" },
+    seedpass: { type: "input", describe: "Admin password", default: "changeme" },
+  },
 });
