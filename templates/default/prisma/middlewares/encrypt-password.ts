58cc3ac863694b3ad19ce7ac29a5d58193dd31c6
Pascal Martineau <pascal@lewebsimple.ca>
2022-07-21 15:10:41 -0400

    fix: typo in prisma middleware (upsert)

--- templates/default/prisma/middlewares/encrypt-password.ts
+++ templates/default/prisma/middlewares/encrypt-password.ts
@@ -4,7 +4,7 @@ import { encryptPassword } from "../../server/utils/password";
 
 // Always encrypt User.password
 export const encryptPasswordMiddleware: Prisma.Middleware = async (params, next) => {
-  if (params.model === "User" && ["usert", "create", "createMany", "update", "updateMany"].includes(params.action)) {
+  if (params.model === "User" && ["create", "createMany", "update", "updateMany", "upsert"].includes(params.action)) {
     await applyRecursive(params.args, "password", (data) => encryptPassword(data.password));
   }
   return next(params);
