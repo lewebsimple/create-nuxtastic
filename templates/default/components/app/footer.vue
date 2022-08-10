8f261334c8111a6b749a6fa788853d507a927ae8
Pascal Martineau <pascal@lewebsimple.ca>
2022-08-10 14:34:44 -0400

    refactor: NavLoginout component

--- templates/default/components/app/footer.vue
+++ templates/default/components/app/footer.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-const { isAuthenticated } = useAuth();
-</script>
-
 <template>
   <div id="app-footer" class="bg-neutral-100 text-neutral-700 py-1.5">
     <div class="container flex justify-end items-center gap-6">
-      <NuxtLink v-if="isAuthenticated" to="/logout" class="link-default"> Log out </NuxtLink>
-      <NuxtLink v-else to="/login" class="link-default"> Log in </NuxtLink>
+      <NavLoginout />
     </div>
   </div>
 </template>
---
+++ templates/default/components/nav/loginout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { isAuthenticated } = useAuth();
+</script>
+
+<template>
+  <NuxtLink v-if="isAuthenticated" to="/logout" class="link-default"> Log out </NuxtLink>
+  <NuxtLink v-else to="/login" class="link-default"> Log in </NuxtLink>
+</template>
